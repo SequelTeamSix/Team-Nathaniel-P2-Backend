577f5343f88c62bc21520271953b7a5b3ca8dd1d
Sfierros <sfierros12@gmail.com>
2022-03-20 16:43:05 -0400

    purchases ready to be tested

--- src/main/java/com/teamnathaniel/service/PurchasesService.java
+++ src/main/java/com/teamnathaniel/service/PurchasesService.java
@@ -1,4 +1,46 @@
 package com.teamnathaniel.service;
 
+import com.teamnathaniel.model.Purchases;
+import com.teamnathaniel.repository.PurchasesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class PurchasesService {
+    PurchasesRepository purchasesRepository;
+
+    @Autowired
+    public PurchasesService(PurchasesRepository purchasesRepository) {
+        this.purchasesRepository = purchasesRepository;
+    }
+
+    public List<Purchases> getAllPurchases(){
+        return purchasesRepository.findAll();
+    }
+
+    public Purchases savePurchase(Purchases purchases){
+        return purchasesRepository.save(purchases);
+    }
+
+    public Purchases findPurchaseById(int purchaseId){
+        return purchasesRepository.findById(purchaseId);
+    }
+
+    public void deletePurchase(int purchaseId){
+        purchasesRepository.deleteByPurchasesId(purchaseId);
+    }
+
+    public Purchases updatePurchase(int purchaseId, Purchases purchases){
+        Purchases oldPurchase = purchasesRepository.findById(purchaseId);
+        if(oldPurchase != null){
+            purchases.setPurchasesId(purchases.getPurchasesId());
+
+            if(purchases.getOrderDate() != null){
+                purchases.setOrderDate(purchases.getOrderDate());
+            }
+        }
+        return purchasesRepository.save(purchases);
+    }
 }
