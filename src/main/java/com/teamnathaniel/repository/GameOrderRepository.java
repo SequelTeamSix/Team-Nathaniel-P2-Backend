044381e417026fd4f6f1d2d7789843331fe995a4
Sfierros <sfierros12@gmail.com>
2022-03-22 18:56:57 -0400

    GameOrder is ready

--- src/main/java/com/teamnathaniel/repository/GameOrderRepository.java
+++ src/main/java/com/teamnathaniel/repository/GameOrderRepository.java
@@ -1,4 +1,23 @@
 package com.teamnathaniel.repository;
 
-public class GameOrderRepository {
+import com.teamnathaniel.model.GameOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface GameOrderRepository extends JpaRepository<GameOrder, Integer> {
+
+    @Query("From GameOrder")
+    List<GameOrder> findAll();
+
+    @Query("from GameOrder where gameOrderId = :gameOrderId")
+    GameOrder findByGameOrderId(int gameOrderId);
+
+    GameOrder save(GameOrder gameOrder);
+
+    void deleteByGameOrderId(int gameOrderId);
 }
--- src/main/java/com/teamnathaniel/service/GameOrderService.java
+++ src/main/java/com/teamnathaniel/service/GameOrderService.java
@@ -1,4 +1,46 @@
 package com.teamnathaniel.service;
 
+import com.teamnathaniel.model.GameOrder;
+import com.teamnathaniel.repository.GameOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class GameOrderService {
+    GameOrderRepository gameOrderRepository;
+
+    @Autowired
+    public GameOrderService(GameOrderRepository gameOrderRepository) {
+        this.gameOrderRepository = gameOrderRepository;
+    }
+
+    public List<GameOrder> getAllGameOrders(){
+        return gameOrderRepository.findAll();
+    }
+
+    public GameOrder saveGameOrder(GameOrder gameOrder){
+        return gameOrderRepository.save(gameOrder);
+    }
+
+    public GameOrder findGameOrderById(int gameOrderId){
+        return gameOrderRepository.findByGameOrderId(gameOrderId);
+    }
+
+    public void deleteGameOrder(int gameOrderId){
+        gameOrderRepository.deleteByGameOrderId(gameOrderId);
+    }
+
+    public GameOrder updateGameOrder(int gameOrderId, GameOrder gameOrder){
+         GameOrder oldGameOrder = gameOrderRepository.findByGameOrderId(gameOrderId);
+         if(oldGameOrder != null){
+             gameOrder.setGameOrderId(gameOrder.getGameOrderId());
+
+             if(gameOrder.getQuantity() != 0){
+                 gameOrder.setQuantity(gameOrder.getGameOrderId());
+             }
+         }
+         return gameOrderRepository.save(gameOrder);
+    }
 }
